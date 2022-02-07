fed2478a501861546d86e81042276c965f3c49c5
Zakary Sisler <zakarysisler@outlook.com>
2022-02-07 15:04:34 -0600

    I created an array and some functions and displayed them on the webpage

---
+++ JavaScriptPlayground/js/main.js
@@ -0,0 +1,88 @@
+﻿//alert() is a method built into browsers that causes an unstylable popup.
+//alert("JavaScript is working!");
+
+//Variables can change values and datatypes (JS isn't type-safe)
+let x = 10;
+
+//We can use console.log() to view the value of a variable in the inspector
+//NOTE: This doesn't show up in the HTML doc, but rather in the console.
+console.log(x);
+
+x = 'Hello';
+console.log(x);
+
+//Var type variables
+var z = 100;
+
+//Var and let difference:
+//let is locally scoped, only available inside the scope it's defined.
+//var is globally scoped, available throughout the entire scope. Var can be re-declared.
+
+//Create a new Date object to display for our copyright
+let currentDate = new Date();// empty constructor for todays date
+
+console.log(currentDate + ': current date')
+
+//Create a variable to take only the year from currentDate
+let currentYear = currentDate.getFullYear();
+
+//JS is a OOP language. Objects have properties which are like nouns and methods are like verbs.
+//methods defined the date class. getFullYear() is an instance method.
+
+console.log(currentYear + '-- current year');
+
+//Use value of currentYear as the <span id="copyright"> content
+//How? Create a variable to capture the span from the HTML document
+let copyright = document.getElementById('copyright'); 
+
+console.log(copyright);
+
+//To update the content of an element, we can use .textContent
+copyright.textContent = currentYear;
+
+
+///////////// Arrays /////////////////////////////////////////////////////////////////
+//JS is LIFO - Last in, First out.
+//JS arrays do not have a set size
+//Can be addressed/accessed like C# with a 0-based index value
+
+let colors = ['blue', 'brown', 'orange', 'pink', 'red', 'green'];
+
+//To add an item to the array we can use push()
+colors.push('purple');
+
+//To remove
+//colors.pop();
+
+//Select a color from the array and use it as the content for our <strong> tag
+//Create a variable for the <strong> tag
+let colorStrong = document.getElementById('yourcolor');
+
+//use .textcontent to give <strong> tag a value
+colorStrong.textContent = colors[3];
+
+//To change the color of the <strong> tag, we can add inline styling with JS
+colorStrong.style.color = colors[3];
+
+//////////////Functions////////////////////////////////////////
+
+//Functions are declared with the "function" keyword
+//The code inside of a function will only run when called upon in reaction to an event (button click, keyload, page load)
+
+//Create a function that will be called when the user submits their fvorite color by clicking a button and display their message to them using thei input
+function displayFavoriteColor() {
+
+    //Create a variable to store the users input
+    let userFavoriteColor = document.getElementById('tbFavColor').value;
+    //Above we used .value after getting the text box element with getElementById().
+    //If you need to get a user's response from a textbox, you MUST use .value with the element.
+
+    //Create a variable for a <span> (output element)
+    let colorMessage = document.getElementById('color-message');
+
+    //Update the <span>'s content to display a message
+    colorMessage.textContent = userFavoriteColor + ' is my favorite color!';
+
+    //Use JS to add inline styling to the <span>
+    colorMessage.style.backgroundColor = userFavoriteColor;
+}
